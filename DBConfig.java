0d1b9d6a8795075109e2b2a75b48814b0040e817
Ahsan Chowdhury Ohi <91822217+Ahsan0ohi@users.noreply.github.com>
2022-06-20 13:25:17 +0600

    3rd Commit
    Added Another object

--- DBConfig.java
+++ DBConfig.java
@@ -1,8 +1,13 @@
 package ECommerce.config;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 
 public class DBConfig {
 	
@@ -13,7 +18,7 @@ public class DBConfig {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		
 		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setUrl
+		dataSource.setUrl(null);
 		dataSource.setUsername(null);
 		dataSource.setPassword(null);
 		
@@ -24,5 +29,29 @@ public class DBConfig {
 		return dataSource;
 		
 	}
+	
+	@Bean(name= "sessionFactory")
+	
+	public SessionFactory getSessionFactory() {
+		
+		Properties hibernateProp = new Properties();
+		
+		hibernateProp.put("hibernateProp.hdm2dd1.auto", "update");
+		hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		
+		LocalSessionFactoryBuilder localFactory = new LocalSessionFactoryBuilder(getH2DataSource());
+		
+		localFactory.addProperties(hibernateProp);
+		
+		
+		
+		System.out.println("--------SessionFactory Object is Created-----------");	
+		
+	    
+		return localFactory.buildSessionFactory();
+		
+	}
+	
+	
 
 }
