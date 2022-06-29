9104650f6b2e6723e648c7697b5497592deec489
Ahsan Chowdhury Ohi <91822217+Ahsan0ohi@users.noreply.github.com>
2022-06-30 01:05:12 +0600

    Implements of UI

---
+++ CategoryUIImpl.java
@@ -0,0 +1,34 @@
+package ECommerce.UI;
+
+import java.util.List;
+
+import ECommerce.model.Category;
+
+public class CategoryUIImpl implements CategoryUI{
+
+	public boolean addCategory(Category category) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteCategory(Category category) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateCategory(Category category) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List<Category> listCategories() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Category getcategory(int categoryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
