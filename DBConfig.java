79edb54ad3352aeee1af0d8b89a803275ea1877b
Ahsan Chowdhury Ohi <91822217+Ahsan0ohi@users.noreply.github.com>
2022-08-20 13:26:23 +0600

    Update DBConfig.java

--- DBConfig.java
+++ DBConfig.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 
 public class DBConfig {
@@ -38,6 +39,7 @@ public class DBConfig {
 		
 		hibernateProp.put("hibernateProp.hdm2dd1.auto", "update");
 		hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+	
 		
 		LocalSessionFactoryBuilder localFactory = new LocalSessionFactoryBuilder(getH2DataSource());
 		
@@ -45,13 +47,24 @@ public class DBConfig {
 		
 		
 		
+		
+		
 		System.out.println("--------SessionFactory Object is Created-----------");	
 		
-	    
 		return localFactory.buildSessionFactory();
+		
 		
 	}
 	
+	
+	@Bean(name="txManager")
+	
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		
+	System.out.println("----------Transaction Manager Object Created--------------");
+	return new HibernateTransactionManager();
+	
+	}
 	
 
 }
